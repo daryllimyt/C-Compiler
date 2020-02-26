00310137c9a20a620271645e9dfa9edd789b327b
daryllimyt <daryllimyt@gmail.com>
2020-02-26 16:06:29 +0000

    added parser skeleton

---
+++ parser/src/parser.cpp
@@ -0,0 +1,12 @@
+#include ""
+int main()
+{
+    const Program *ast=parseAST();
+    std::cout << "<?xml version=\"1.0\"?>" << '\n';
+    std::cout << "<Program>" << '\n';
+    ast->print();
+    std::cout << "</Program>" << '\n';
+    std::cout<<std::endl;
+
+    return 0;
+}
