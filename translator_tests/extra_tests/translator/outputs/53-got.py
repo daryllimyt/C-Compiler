2e967b4646d5ab2cbb5875c1ca4925385d2d2e22
daryllimyt <daryllimyt@gmail.com>
2020-03-18 19:43:09 +0000

    cleaned 84/100

--- translator_tests/extra_tests/translator/outputs/53-got.py
+++
@@ -1,9 +0,0 @@
-def main():
-	x = 1.3
-	return x
-
-
-if __name__ == "__main__":
-	import sys
-	ret=main()
-	sys.exit(ret)
--- translator_tests/extra_tests/translator/outputs/59-got.py
+++
@@ -1,9 +0,0 @@
-def main():
-	x =  - 10
-	return  - 10
-
-
-if __name__ == "__main__":
-	import sys
-	ret=main()
-	sys.exit(ret)
