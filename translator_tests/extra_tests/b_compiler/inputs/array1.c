77cef95505a905d8aea002372c57f793b4323392
daryllimyt <daryllimyt@gmail.com>
2020-03-30 05:34:36 +0100

    a

--- translator_tests/extra_tests/b_compiler/inputs/array1.c
+++ translator_tests/extra_tests/b_compiler/inputs/array1.c
@@ -1,20 +1,19 @@
 // int sub(int i, int j, int k);
 
-int sub(int i, int j, int k) {
-    i -= j;
-    i -= k;
-    return i;
-}
-
 int fmain() {
-    // int x = 3;
+    int x = 3;
     int arr[3];
-    // arr[0] = 100 + x - 3;
-    arr[1] = sub(4, 1, 2); // returns 1
-    // arr[3] = arr[1] + 20; // 21
-    // arr[2] = arr[3]--; // 20
-    // int a = arr[0] + arr[1]; // 101
-    // return a + arr[3] + arr[2]; // return 142
-    return 0;
+    arr[0] = 100 + x - 3;
+    // arr[1] = sub(4, 1, 2); // returns 1
+    arr[1] = 1;
+    arr[3] = arr[1] + 20; // 21
+    arr[2] = arr[3]--; // 20
+    int a = arr[0] + arr[1]; // 101
+    return a + arr[3] + arr[2]; // return 142
 }
 
+// int sub(int i, int j, int k) {
+//     i -= j;
+//     i -= k;
+//     return i;
+// }
